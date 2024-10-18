6b02cccede86bbe85401518d899c0e9a47064290
slymackjr <jbnyamasheki@gmail.com>
2024-10-18 06:00:37 +0300

    updated the subdomain to working one

--- src/Hooks/Axios.jsx
+++ src/Hooks/Axios.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://asa.co.tz/api',
+  baseURL: 'https://api.asa.co.tz/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
