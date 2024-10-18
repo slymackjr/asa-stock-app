1f4b5913a8bb57dcf37de48d17a79bc9a250fe88
slymackjr <jbnyamasheki@gmail.com>
2024-10-18 05:22:58 +0300

    configuration for deployment where done

--- src/Hooks/Axios.jsx
+++ src/Hooks/Axios.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://asa.co.tz/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
